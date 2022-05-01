b948829bf8025bd683ee68ca842f130eb9132e6e
Kenivia <kenivia.fan@gmail.com>
2022-05-01 12:52:54 +1200

    only one instance of pysync can run at a time

--- pysync/__init__.py
+++ pysync/__init__.py
@@ -11,10 +11,17 @@ from pysync.Options_parser import check_options
 
 
 def main():
-
+    try:
+        import socket
+        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        # Create an abstract socket, by prefixing it with null.
+        s.bind('\0pysync_process_lock')
+    except socket.error:
+        print("an instance of pysync is already running. Exiting")
+        sys.exit(0)
+        
     check_options()
 
-    timer = None
     print("pysync started successfully")
     try:
         timer = event_sequence(load_options("PATH"))
